fa9f42be09fe0ec2aec8cd1e778ef546462c25f2
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-06 11:50:12 -0500

    en data firebase auth y firestore

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -12,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db }
