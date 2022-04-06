ceb0aa170a88479250b1040ec99f7b1977cc053b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-06 00:27:07 -0500

    se añaden assets a la data products

--- src/data/products.js
+++ src/data/products.js
@@ -2,16 +2,16 @@ const dataProducts = [
 
     {
         slug: 1,
-        name: "",
-        photo: "",
+        name: "aspersor-estacionario-giratorio-de-3-brazos",
+        photo: require('../assets/aspersor-estacionario-giratorio-de-3-brazos-surtek-130347.jpg'),
         description: "",
-        reference: "",
-        price: "",
+        reference: "0-7f54",
+        price: 104.000,
     },
     {
         slug: 2,
         name: "",
-        photo: "",
+        photo: require('../assets/aspiradora-industrial-21l-solidos-y-liquidos-vc2512l.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -19,7 +19,7 @@ const dataProducts = [
     {
         slug: 3,
         name: "",
-        photo: "",
+        photo: require('../assets/aspiradora-karcher-multiuso-wd3-mx.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -27,7 +27,7 @@ const dataProducts = [
     {
         slug: 4,
         name: "",
-        photo: "",
+        photo: require('../assets/bolsa-de-herramientas-stanley-16x9x12-pulg-stst516126.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -35,7 +35,7 @@ const dataProducts = [
     {
         slug: 5,
         name: "",
-        photo: "",
+        photo: require('../assets/calibrador-tipo-rampa-para-bujia-107336-surtek.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -43,7 +43,7 @@ const dataProducts = [
     {
         slug: 6,
         name: "",
-        photo: "",
+        photo: require('../assets/candado-ifam-arco-de-nylon-serie-safety-color-rojo-cod044722n1.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -51,7 +51,7 @@ const dataProducts = [
     {
         slug: 7,
         name: "",
-        photo: "",
+        photo: require('../assets/centro-puntos.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -59,7 +59,7 @@ const dataProducts = [
     {
         slug: 8,
         name: "",
-        photo: "",
+        photo: require('../assets/cepillo-global-stanley-12175.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -67,7 +67,7 @@ const dataProducts = [
     {
         slug: 9,
         name: "",
-        photo: "",
+        photo: require('../assets/cincel-plano-34pulg-sds-plus-truper-sds-c1.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -75,7 +75,7 @@ const dataProducts = [
     {
         slug: 10,
         name: "",
-        photo: "",
+        photo: require('../assets/esmeril-dewalt-profesional-de-banco-de-6.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -83,7 +83,7 @@ const dataProducts = [
     {
         slug: 11,
         name: "",
-        photo: "",
+        photo: require('../assets/juego-4-alicates-y-picoloro-pretul-jgo-pin5.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -91,7 +91,7 @@ const dataProducts = [
     {
         slug: 12,
         name: "",
-        photo: "",
+        photo: require('../assets/juego-de-bisturies-hobby-pretul-exa-13p.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -99,7 +99,7 @@ const dataProducts = [
     {
         slug: 13,
         name: "",
-        photo: "",
+        photo: require('../assets/juego-de-botadores.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -107,7 +107,7 @@ const dataProducts = [
     {
         slug: 14,
         name: "",
-        photo: "",
+        photo: require('../assets/juego-de-copas-85-piezas-de-14-a-38-stanley.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -115,7 +115,7 @@ const dataProducts = [
     {
         slug: 15,
         name: "",
-        photo: "",
+        photo: require('../assets/juego-destornilladores-joyero-x-43pzs-truper-joy-43.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -123,7 +123,7 @@ const dataProducts = [
     {
         slug: 16,
         name: "",
-        photo: "",
+        photo: require('../assets/martillo-con-mango-de-madera-cabeza-conica-stanley-51271.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -131,7 +131,7 @@ const dataProducts = [
     {
         slug: 17,
         name: "",
-        photo: "",
+        photo: require('../assets/pistola-de-soldar-2-temperaturas-140w-pretul-cau-140.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -139,7 +139,7 @@ const dataProducts = [
     {
         slug: 18,
         name: "",
-        photo: "",
+        photo: require('../assets/prensa-de-coordenadas-winwork-no-6-150-mm.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -147,7 +147,7 @@ const dataProducts = [
     {
         slug: 19,
         name: "",
-        photo: "",
+        photo: require('../assets/probador-de-correinte-tester-20cm-ccaiman-truper-proco-2ox.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -155,7 +155,7 @@ const dataProducts = [
     {
         slug: 20,
         name: "",
-        photo: "",
+        photo: require('../assets/pulidora-stanley-9-230mm-2200w-stgl2223-b3.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -163,7 +163,7 @@ const dataProducts = [
     {
         slug: 21,
         name: "",
-        photo: "",
+        photo: require('../assets/sistema-aspiracion-polvo-gde-18v-16-perforaciones-gbh-18v-26.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -171,7 +171,7 @@ const dataProducts = [
     {
         slug: 22,
         name: "",
-        photo: "",
+        photo: require('../assets/taladro-percutor-38-atornil-36v-black-decker-tb550-b3a.jpg'),
         description: "",
         reference: "",
         price: "",
@@ -179,21 +179,11 @@ const dataProducts = [
     {
         slug: 23,
         name: "",
-        photo: "",
+        photo: require('../assets/torquimetro-de-trueno-38-120-960-in-lb-surtek-107102.jpg'),
         description: "",
         reference: "",
         price: "",
     },
-    {
-        slug: 24,
-        name: "",
-        photo: "",
-        description: "",
-        reference: "",
-        price: "",
-    },
-
-
 ];
 
 export { dataProducts }
