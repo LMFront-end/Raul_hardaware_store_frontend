89f79d1699213fd49a5508ba390201829c94dec8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-06 00:17:12 -0500

    se crea footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <section className="social">      
+            <p>Follow TechNews</p>      
+            <div className="links">    
+
+            <a href="https://facebook.com">          
+            <i className="fab fa-facebook-f"></i>        
+            </a>        
+            
+            <a href="https://twitter.com">          
+            <i className="fab fa-twitter"></i>        
+            </a>        
+            
+            <a href="https://linkdin.com">          
+            <i className="fab fa-linkedin"></i>        
+            </a>      
+            </div>    
+        </section>
+
+        {/* Footer links */}
+
+        <div className="footer-links">    
+            <div className="footer-container">      
+                <ul>        
+                    <li>          
+                        <a href="#">            
+                        <h3>Title One</h3>          
+                        </a>    
+                    </li>        
+                    <li>          
+                        <a href="#">Blockchain</a>        
+                    </li>
+
+                    <li>          
+                        <a href="#">Machine Learning</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Artificial Intelligence</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Quantum Computing</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Startup</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Cryptocurrencies</a>        
+                    </li>      
+                </ul>      
+            <ul>        
+                <li>          
+                    <a href="#">            
+                    <h3>Title Two</h3>          
+                    </a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Blockchain</a>       
+                    </li> 
+
+                    <li>          
+                        <a href="#">Machine Learning</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Artificial Intelligence</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Quantum Computing</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Startup</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Cryptocurrencies</a>        
+                    </li>      
+                </ul>      
+            <ul>        
+                <li>          
+                    <a href="#">            
+                    <h3>Title Three</h3>          
+                </a>        
+                </li>        
+                <li>          
+                    <a href="#">Blockchain</a>  
+                </li>        
+                <li>          
+                    <a href="#">Machine Learning</a>        
+                </li>        
+                <li>         
+                    <a href="#">Artificial Intelligence</a>        
+                </li>        
+                <li>          
+                    <a href="#">Quantum Computing</a>        
+                </li>        
+                <li>          
+                    <a href="#">Startup</a>        
+                </li>        
+                <li>          
+                    <a href="#">Cryptocurrencies</a>        
+                </li>      
+                </ul>      
+                <ul>        
+                    <li>          
+                        <a href="#">            
+                        <h3>Title Four</h3>          
+                        </a>        
+                    </li> 
+                    <li>
+                        <a href="#">Blockchain</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Machine Learning</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Artificial Intelligence</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Quantum Computing</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Startup</a>        
+                    </li>        
+                    <li>          
+                        <a href="#">Cryptocurrencies</a>        
+                    </li>      
+                </ul>    
+                </div>  
+                </div>
+                {/* Footer */}
+
+                <footer className="footer">    
+                <h3>Tech News Copyright</h3>  
+                </footer>
+    
+    </>
+  )
+}
+
+export {Footer}
