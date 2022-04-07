4545d9a2734078066c8fcea054ec35c2cee82cdc
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-07 15:10:26 -0500

    se crea page hardaware y se instala bootstrap

---
+++ src/pages/hardwareStore/HardwareStore.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HardwareStore = () => {
+  return (
+    <>
+    <div className="hero">
+            <div className="card bg-dark text-white image_home">
+                <img className="welcome"/>
+            <div className="card-img-overlay d-flex flex-column justify-content-center">
+
+                <div className="container">
+
+                    <h5 className="card-title display-3 fw-bolder mb-0">STYLE, SELF-LOVE, INSPIRATION</h5>
+
+                    <p className="card-text lead fs-2">
+                        CHECK OUT THIS WEEK'S TOP SELLERS
+                    </p>
+                </div>
+            </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default HardwareStore
