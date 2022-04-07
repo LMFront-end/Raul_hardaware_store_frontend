6eb6cd6da7f8e2b62355bc7f975089d0ce9e65e0
LMFront-end <perfilgitlinamaria@gmail.com>
2022-04-07 01:50:41 -0500

    se implememtan url_api

--- src/components/tableCustomer/CustomerTable.jsx
+++ src/components/tableCustomer/CustomerTable.jsx
@@ -3,14 +3,10 @@ import MaterialTable from 'material-table'
 
 const CustomerTable = () => {
 
-<<<<<<< HEAD
   const [tableData, setTableData] = useState([]);
 
-  const columns = [
-
-=======
   const columns =  [
->>>>>>> 256bc9ae3054bfb962d13286177dfd247091f989
+
     {
       title: "Id",
       field: "id"
@@ -20,34 +16,24 @@ const CustomerTable = () => {
       field: "name"
     },
     {
-<<<<<<< HEAD
+
       title: "Phone Number",
-=======
+
       title: "PhoneNumber",
->>>>>>> 256bc9ae3054bfb962d13286177dfd247091f989
+
       field: "phone Number"
     },
     {
       title: "Identity Document",
       field: "identity Document"
-<<<<<<< HEAD
+
     }
-  ]
-
-  return (
-    <>
-    <MaterialTable title="Customer" columns={columns} data={tableData} />
-=======
-    },
-
   ]
 
 
   return (
     <>
       <MaterialTable columns={columns} />
-    
->>>>>>> 256bc9ae3054bfb962d13286177dfd247091f989
     </>
   )
 }
